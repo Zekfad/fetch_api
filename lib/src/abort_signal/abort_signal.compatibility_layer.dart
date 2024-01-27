ecfd646f52cbba7290f067aca9cce28b0e81b07e
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2024-01-27 23:06:06 +0300

    feat!: remove compatibility layer

--- lib/src/abort_signal/abort_signal.compatibility_layer.dart
+++
@@ -1,7 +0,0 @@
-part of 'abort_signal.dart';
-
-
-/// Returns an [AbortSignal] instance that will automatically abort
-/// after a specified [time].
-AbortSignal createAbortSignalTimeout(Duration time)
-  => AbortSignal._timeout(time.inMilliseconds);
