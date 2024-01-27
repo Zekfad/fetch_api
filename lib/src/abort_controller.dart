80db20a75ba1b67e35c2c3ab3bfd92411877c80d
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2024-01-27 23:08:44 +0300

    feat: rewrite abort controller and signal

--- lib/src/abort_controller.dart
+++ lib/src/abort_controller.dart
@@ -1,26 +1,27 @@
-import '_js.dart';
+import 'dart:js_interop';
+
 import 'abort_signal.dart';
 
 
 /// The [AbortController] interface represents a controller object that allows
 /// you to abort one or more Web requests as and when desired.
 /// 
-/// You can create a new [AbortController] object using the `AbortController()`
-/// constructor. Communicating with a DOM request is done using an [AbortSignal]
-/// object.
+/// You can create a new [AbortController] object using
+/// the [AbortController.new] constructor.
 @JS()
-@staticInterop
-class AbortController {
+extension type AbortController<T extends JSAny>._(JSObject _) implements JSObject {
   /// Creates a new [AbortController] object instance.
+  @JS('AbortController')
   external factory AbortController();
-}
 
-extension AbortControllerInstanceMembers on AbortController {
-  /// Returns an [AbortSignal] object instance, which can be used to
-  /// communicate with, or to abort, a DOM request.
-  external final AbortSignal signal;
+  /// Returns an [AbortSignal] object instance, which can be used
+  /// to communicate with, or to abort, an asynchronous operation.
+  @JS()
+  external final AbortSignal<T> signal;
 
-  /// Aborts a DOM request before it has completed. This is able to abort
-  /// fetch requests, consumption of any response bodies, and streams.
-  external void abort([dynamic reason]);
+  /// Aborts an asynchronous operation before it has completed.
+  /// This is able to abort fetch requests, consumption of any response bodies,
+  /// and streams.
+  @JS()
+  external void abort([T? reason]);
 }
--- lib/src/abort_signal.dart
+++ lib/src/abort_signal.dart
@@ -1,1 +1,67 @@
-export 'abort_signal/abort_signal.dart' hide createAbortSignalTimeout;
+import 'dart:js_interop';
+
+import 'abort_controller.dart';
+import 'dom_exception.dart';
+
+
+/// The [AbortSignal] interface represents a signal object that allows you
+/// to communicate with an asynchronous operation (such as a fetch request)
+/// and abort it if required via an [AbortController] object.
+@JS()
+extension type AbortSignal<T extends JSAny>._(JSObject _) implements JSObject {
+  /// The [AbortSignal.abort] static method returns an [AbortSignal] that is
+  /// already set as aborted (and which does not trigger an `abort` event).
+  static AbortSignal<T> abort<T extends JSAny>([T? reason]) =>
+    _abort<T>(reason);
+
+  /// The [AbortSignal.any] static method takes an [iterable] of abort signals
+  /// and returns an [AbortSignal].
+  /// The returned abort signal is aborted when any of the input iterable
+  /// abort signals are aborted.
+  /// The abort reason will be set to the reason of the first signal
+  /// that is aborted. If any of the given abort signals are already aborted
+  /// then so will be the returned [AbortSignal].
+  static AbortSignal<T> any<T extends JSAny>(List<AbortSignal<T>> iterable) =>
+    _any<T>(iterable.toJS);
+
+
+  /// The [AbortSignal.timeout] static method returns an [AbortSignal]
+  /// that will automatically abort after a specified [time].
+  /// 
+  /// The signal aborts with a `TimeoutError` [DOMException] on timeout,
+  /// or with `AbortError` [DOMException] due to pressing a browser
+  /// stop button (or some other inbuilt "stop" operation).
+  static AbortSignal<DOMException> timeout(Duration time) =>
+    _timeout(time.inMilliseconds);
+
+  @JS('abort')
+  external static AbortSignal<T> _abort<T extends JSAny>([T? reason]);
+
+  @JS('any')
+  external static AbortSignal<T> _any<T extends JSAny>(JSArray<AbortSignal<T>> iterable);
+
+  @JS('timeout')
+  external static AbortSignal<DOMException> _timeout(int time);
+
+  /// The [throwIfAborted] method throws the signal's abort [reason]
+  /// if the signal has been aborted; otherwise it does nothing.
+  @JS()
+  external void throwIfAborted();
+
+  /// The [aborted] read-only property returns a value that indicates whether
+  /// the asynchronous operations the signal is communicating with are aborted
+  /// (`true`) or not (`false`).
+  @JS()
+  external bool get aborted;
+
+  /// The [reason] read-only property returns a JavaScript value that indicates
+  /// the abort reason.
+  /// 
+  /// The property is `undefined` when the signal has not been aborted.
+  /// It can be set to a specific value when the signal is aborted,
+  /// using [AbortController.abort] or [AbortSignal.abort].
+  /// If not explicitly set in those methods, it defaults to `AbortError`
+  /// [DOMException].
+  @JS()
+  external T? get reason;
+}
