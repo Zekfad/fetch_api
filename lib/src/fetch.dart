cffbff9553031d5e474ef6cf428a9ad5ecc8a713
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2024-01-29 02:27:24 +0300

    feat!: use new api

--- lib/src/fetch.dart
+++ lib/src/fetch.dart
@@ -1,10 +1,11 @@
-import '_js.dart';
-import 'fetch_options.dart';
+import 'dart:js_interop';
+
+import 'request_options.dart';
 import 'response.dart';
 
 
 @JS('fetch')
-external Promise<Response> _fetch(dynamic resource, FetchOptions? options);
+external JSPromise<Response> _fetch(JSAny resource, [ RequestInit? options, ]);
 
 /// The global `fetch()` method starts the process of fetching a resource
 /// from the network, returning a promise which is fulfilled once the response
@@ -16,8 +17,8 @@ external Promise<Response> _fetch(dynamic resource, FetchOptions? options);
 /// A `fetch()` promise only rejects when a network error is encountered
 /// (which is usually when there's a permissions issue or similar).
 /// A `fetch()` promise does not reject on HTTP errors (`404`, etc.).
-/// Instead, a then() handler must check the [ResponseInstanceMembers.ok] and/or
-/// [ResponseInstanceMembers.status] properties.
+/// Instead, a then() handler must check the [Response.ok] and/or
+/// [Response.status] properties.
 /// 
 /// `WindowOrWorkerGlobalScope` is implemented by both `Window` and
 /// `WorkerGlobalScope`, which means that the `fetch()` method is available
@@ -26,5 +27,14 @@ external Promise<Response> _fetch(dynamic resource, FetchOptions? options);
 /// The `fetch()` method is controlled by the `connect-src` directive of
 /// Content Security Policy rather than the directive of the resources
 /// it's retrieving.
-Future<Response> fetch(dynamic resource, [FetchOptions? options]) =>
-  promiseToFuture(_fetch(resource, options));
+Future<Response> fetch(Object resource, [ RequestInit? options, ]) =>
+  _fetch(
+    switch (resource) {
+      String() => resource.toJS,
+      Uri() => resource.toString().toJS,
+      // Always matched due to JS type erasure
+      JSObject() => resource,
+      _ => throw StateError('Invalid resource type: ${resource.runtimeType}'),
+    },
+    options ?? FetchOptions(),
+  ).toDart;
